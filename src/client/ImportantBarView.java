626afcf969a7bbce7de6f48b5459eec64707fef7
nicSt12 <nico.straeuli@hotmail.com>
2021-10-22 18:12:29 +0200

    Added code to apply CSS-Stylsheet

--- src/client/ImportantBarView.java
+++ src/client/ImportantBarView.java
@@ -24,6 +24,12 @@ public class ImportantBarView  extends MainBarView {
 
 		// Gets items of ObservableArrayList from method getToDoListImportant
 		this.tableView.getItems().addAll(toDoListImportant);
+		
+		// Add CSS styling
+		this.getStylesheets().add(getClass().getResource("CategoryViewStyle.css").toExternalForm());
+		this.icon.getStyleClass().add("icon");
+		this.label.getStyleClass().add("label");
+		this.header.getStyleClass().add("header");
 }
 		
 	}
