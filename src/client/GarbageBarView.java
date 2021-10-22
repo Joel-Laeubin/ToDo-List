89f8c8a88b32b46caddf382e0ac4ef253e275006
nicSt12 <nico.straeuli@hotmail.com>
2021-10-22 18:12:34 +0200

    Added code to apply CSS-Stylsheet

--- src/client/GarbageBarView.java
+++ src/client/GarbageBarView.java
@@ -25,6 +25,12 @@ public class GarbageBarView  extends MainBarView {
 		
 		// Gets items of ObservableArrayList from method getToDoListImportant
 		this.tableView.getItems().addAll(toDoListGarbage);
+		
+		// Add CSS styling
+		this.getStylesheets().add(getClass().getResource("CategoryViewStyle.css").toExternalForm());
+		this.icon.getStyleClass().add("icon");
+		this.label.getStyleClass().add("label");
+		this.header.getStyleClass().add("header");
 	}
 	
 
