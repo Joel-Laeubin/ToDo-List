59dee38266f8eb4aa0a45032d72c9242b4fb11f3
mkfhnw <margareta.karaqi@students.fhnw.ch>
2021-11-01 23:27:02 +0100

    comments

--- src/model/FocusTimerModel.java
+++ src/model/FocusTimerModel.java
@@ -7,29 +7,44 @@ import javafx.scene.control.Label;
 
 public class FocusTimerModel {
 	
-	 private boolean isRunning = false;
-	    private boolean isBreak = false;
-	    private Timer timer = new Timer();
-	    private int counter = 60 * 25; // 60 * 25 Testing:10
+		// Elements
+	 	private boolean isRunning;
+	    private boolean isBreak;
+	    private Timer timer;
+	    private int counter;
 	    private final Label counterLabel;
 	    private int seconds, minutes;
-
+	    
+	    // Constructor
 	    public FocusTimerModel (Label counterLabel) {
 			this.counterLabel = counterLabel;
+			
+			this.isRunning = false;
+			this.isBreak = false;
+			this.timer = new Timer();
+			this.counter = 60 * 25; // 25 minutes * 60 = 1500 seconds
+						
 	    }
 
+	    // Shows if timer is currently running
 	    public boolean isRunning() {
 	        return isRunning;
 	    }
-
+	    
+	    // Runs timer
 	    public void setRunning(boolean running) {
 	        isRunning = running;
 	    }
-
+	    
+	    // Pauses timer
 	    public void pause() {
 	        timer.cancel();
 	    }
 
+	    /*
+	     * --LOGIC--
+	     * Starts timer and changes minutes/seconds
+	     */
 	    public void start() {
 	        timer = new Timer();
 	        timer.scheduleAtFixedRate(new TimerTask() {
@@ -51,6 +66,7 @@ public class FocusTimerModel {
 	        }, 0, 1500);
 	    }
 	    
+	    // Sets timer back to 25 minutes and starts running again
 	    public void restart() {
 	    	isRunning();
 	    	setRunning(true);
@@ -86,6 +102,7 @@ public class FocusTimerModel {
 	    }
 	    	
 
+	    // Getter and Setter
 		public boolean isBreak() {
 			return isBreak;
 		}
